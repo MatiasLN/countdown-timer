10eabc4cd786729c3d271c60b1c6845c54243fc3
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2023-07-18 12:32:41 +0200

    La til UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import { ViewSwitch } from "./components/ViewSwitch";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+const targetTime = moment("2023-09-18");
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(Date.now());
+
+  const timeBetween = targetTime - currentTime;
+  const seconds = Math.floor((timeBetween / 1000) % 60);
+  const minutes = Math.floor((timeBetween / 1000 / 60) % 60);
+  const hours = Math.floor((timeBetween / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(timeBetween / (1000 * 60 * 60 * 24));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(Date.now());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="app">
-      <ViewSwitch />
+      <div className="eirik"></div>
+      <h1>Eirik er tilbake på kontoret om</h1>
+      <p className="counter">
+        <span>{days} dager </span>
+        <span>{hours} timer </span>
+        <span>{minutes} minutter </span>
+        <span>{seconds} sekunder</span>
+      </p>
+      <div className="emoji"></div>
     </div>
   );
 }
